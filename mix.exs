6ef79eea52df161c485401f7a460f01eab5656ff
Roman <rastopyr@gmail.com>
2017-10-09 21:46:46 +0300

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule {{MODULE_NAME}}.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :{{APP_NAME}},
+      version: "0.0.1",
+      elixir: "~> {{MIX_VERSION_SHORT}}",
+      elixirc_paths: ["lib"],
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [
+      mod: { {{MODULE_NAME}}, [] },
+      applications: [:logger]
+    ]
+  end
+
+  # Returns the list of dependencies in the format:
+  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
+  #
+  # To specify particular versions, regardless of the tag, do:
+  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
+  defp deps do
+    []
+  end
+end
